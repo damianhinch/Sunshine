73702e81762f77b1ac6a91fd511ab17253483935
damianhinch <damianhinch@gmail.com>
2015-03-19 17:04:57 +0100

    Add DetailView

---
+++ app/src/main/java/com/damianhinch/sunshine/Helpers.java
@@ -0,0 +1,24 @@
+package com.damianhinch.sunshine;
+
+import java.text.SimpleDateFormat;
+
+public class Helpers {
+
+
+    public static String getReadableDateString(long time) {
+        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+        return shortenedDateFormat.format(time);
+    }
+
+    /**
+     * Prepare the weather high/lows for presentation.
+     */
+    public static String formatHighLows(double high, double low) {
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        long roundedHigh = Math.round(high);
+        long roundedLow = Math.round(low);
+
+        String highLowStr = roundedHigh + "/" + roundedLow;
+        return highLowStr;
+    }
+}
