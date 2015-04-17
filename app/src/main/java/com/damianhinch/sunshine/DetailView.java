decd036cfb24632ebf5c76f3af19e8c87db84685
damianhinch <damianhinch@gmail.com>
2015-04-17 12:06:30 +0200

    Add public content provider and remove unnecessary comments

--- app/src/main/java/com/damianhinch/sunshine/DetailView.java
+++ app/src/main/java/com/damianhinch/sunshine/DetailView.java
@@ -35,26 +35,6 @@ public class DetailView extends ActionBarActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detailfragment, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -97,16 +77,12 @@ public class DetailView extends ActionBarActivity {
 
         @Override
         public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-            // Inflate the menu; this adds items to the action bar if it is present.
             inflater.inflate(R.menu.detailfragment, menu);
 
-            // Retrieve the share menu item
             MenuItem menuItem = menu.findItem(R.id.action_share);
 
-            // Get the provider and hold onto it to set/change the share intent.
             mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
-            // If onLoadFinished happens before this, we can go ahead and set the share intent now.
             if (mForecast != null) {
                 mShareActionProvider.setShareIntent(createShareForecastIntent());
             }
