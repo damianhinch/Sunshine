373f177d03ce4eb39a8a535f24b00fc879e2431e
damianhinch <damianhinch@gmail.com>
2015-04-17 14:47:20 +0200

    Stop DetailView from breaking due to new designs

--- app/src/main/java/com/damianhinch/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/damianhinch/sunshine/ForecastAdapter.java
@@ -55,10 +55,7 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // our view is pretty simple here --- just a text view
-        // we'll keep the UI functional with a simple (and slow!) binding.
-
-        TextView tv = (TextView) view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+//        TextView tv = (TextView) view;
+//        tv.setText(convertCursorRowToUXFormat(cursor));
     }
 }
